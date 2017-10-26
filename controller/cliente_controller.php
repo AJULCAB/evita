acc24c7439ee200f6b856658a1b4774907344bdb
AHIEZER JULCA <julca.blas@gmail.com>
2017-10-26 01:31:28 -0500

    SUBIDA MODIFICADAaaaa

--- controller/cliente_controller.php
+++ controller/cliente_controller.php
@@ -1,6 +1,9 @@
-<?php 
-
-
-
-
- ?>
+<?php
+include '../model/cliente_model.php'
+$dni_cliente = $_REQUEST["dni"];
+$nom_cliente = $_REQUEST["nombre"];
+$app_cliente = $_REQUEST["apellido"];
+$distrito    = $_REQUEST["distrito"];
+$telefono    = $_REQUEST["telefono"];
+$correo      = $_REQUEST["correo"];
+$tipo        = $_REQUEST["tipo"];
