1fd063ad027da9d306730944b9c772c05be55a5d
AHIEZER JULCA <julca.blas@gmail.com>
2017-10-26 17:36:55 -0500

    adjfhkeg+

--- template/evita/index.php
+++ template/evita/index.php
@@ -1,12 +1,42 @@
 <?php include_once('header.php'); ?>
+ <div class="container">
 
-	<div class="col-md-6">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default navbar-negative">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Librería Evita</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Conocenos</a></li>
+              <li><a href="#">Contáctenos</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Productos <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Útiles</a></li>
+                  <li><a href="#">Libros</a></li>
+                  <li><a href="#">Cuadernos</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Login<span class="sr-only">(current)</span></a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+      <div class="jumbotron">
 
+    
+
+      </div>
+
+    </div> <!-- /container -->
 <?php include_once('footer.php'); ?>
